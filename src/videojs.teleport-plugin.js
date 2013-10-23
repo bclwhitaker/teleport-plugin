ba15f4e27a8aa5a664fbdc0f24784798450cabc5
Lee Whitaker <lwhitaker@brightcove.com>
2013-10-23 12:29:46 -0400

    save time on pause for now

--- src/videojs.teleport-plugin.js
+++ src/videojs.teleport-plugin.js
@@ -35,13 +35,11 @@
       var
         // save a reference to the player instance
         player = this,
+        user = '12345', //unique user identifier
+        videoId = '010101', //it of video being watched
 
         // merge options and defaults
         settings = extend({}, defaults, options || {});
-
-      player.on('play', function(){
-        console.log('play');
-      });
 
       // replace the initializer with the plugin functionality
       player.teleportplugin = {
@@ -53,8 +51,36 @@
         },
         extreme: function() {
           return 'awesome!';
+        },
+        savePosition: function() {
+          var xmlHttp = null;
+          saveUrl = 'http://ec2-107-20-72-18.compute-1.amazonaws.com/set/'+user+'/'+videoId+'/'+player.currentTime();
+          xmlHttp = new XMLHttpRequest();
+          xmlHttp.open( "GET", saveUrl, false );
+          xmlHttp.send( null );
+          return;
+        },
+        savedPosition: function() {
+          var xmlHttp = null;
+          savedPositionUrl = 'http://ec2-107-20-72-18.compute-1.amazonaws.com/set/'+user+'/'+videoId+'/';
+          xmlHttp = new XMLHttpRequest();
+          xmlHttp.open( "GET", savedPositionUrl, false );
+          xmlHttp.send( null );
+          return xmlHttp.responseText;
         }
       };
+
+      player.on('play', function() {
+        console.log('play');
+      });
+
+      player.on('progress', function(){
+
+      });
+
+      player.on('pause', function() {
+        player.teleportplugin.savePosition();
+      });
     };
   
   // register the plugin with video.js
